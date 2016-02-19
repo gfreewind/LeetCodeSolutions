2fbbab1cc38c6b072fe5444940561a7743ab3b03
Gao Feng <gfree.wind@outlook.com>
2016-02-19 15:31:50 +0800

    Enhance 4. Median of Two Sorted Arrays with k/2 method

--- 4-Median of Two Sorted Arrays/4-Median of Two Sorted Arrays/4-Median of Two Sorted Arrays.cpp
+++ 4-Median of Two Sorted Arrays/4-Median of Two Sorted Arrays/4-Median of Two Sorted Arrays.cpp
@@ -10,6 +10,53 @@
 using namespace std;
 
 class Solution {
+private:
+	int find_kth_num(int *nums1, int size1, int *nums2, int size2, int k)
+	{
+		if (size1 > size2) {
+			return find_kth_num(nums2, size2, nums1, size1, k);
+		}
+
+		if (0 == size1) {
+			return nums2[k-1];
+		}
+
+		if (1 == k) {
+			return min(nums1[0], nums2[0]);
+		}
+
+		int p1 = min(k / 2, size1);
+		int p2 = k - p1;
+		int n1 = nums1[p1-1];
+		int n2 = nums2[p2-1];
+
+		if (n1 < n2) {
+			return find_kth_num(nums1 + p1, size1 - p1, nums2, size2, k - p1);
+		}
+		else if (n1 > n2) {
+			return find_kth_num(nums1, size1, nums2 + p2, size2 - p2, k - p2);
+		}
+		else {
+			return n1;
+		}
+	}
+public:
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+		int total = nums1.size() + nums2.size();
+		int middle = total / 2;
+
+		if (total & 1) {
+			return find_kth_num(nums1.data(), nums1.size(), nums2.data(), nums2.size(), middle+1);
+		}
+		else {
+			int n1 = find_kth_num(nums1.data(), nums1.size(), nums2.data(), nums2.size(), middle);
+			int n2 = find_kth_num(nums1.data(), nums1.size(), nums2.data(), nums2.size(), middle+1);
+			return (double)(n1 + n2) / 2;
+		}
+	}
+};
+
+class MergeSolution {
 public:
 	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 		if (nums1.size() == 0) {
@@ -82,8 +129,11 @@ public:
 static void test_func(vector<int> &v1, vector<int> &v2)
 {
 	Solution s;
+	MergeSolution s2;
 	double ret = s.findMedianSortedArrays(v1, v2);
-	cout << "Result is " << ret << endl;
+	cout << "Result1 is " << ret << endl;
+	ret = s2.findMedianSortedArrays(v1, v2);
+	cout << "Result2 is " << ret << endl;
 }
 
 int main()
